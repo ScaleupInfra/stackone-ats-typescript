39250fbe01e42fd8e32c484fd4ae77ebbeb2a47a
anuragkainth <anuragkainth07@gmail.com>
2024-08-05 18:06:13 +0530

    feat: backend endpoints added

---
+++ backend/src/config.ts
@@ -0,0 +1,9 @@
+import { config } from 'dotenv';
+config();
+
+export default {
+  PORT: process.env.PORT || 3001,
+  STACKONE_API_KEY: Buffer.from(process.env.STACKONE_API_KEY || "").toString('base64'),
+  STACKONE_BASE_URL: 'https://api.stackone.com',
+  STACKONE_ATS_URL: 'https://api.stackone.com/unified/ats'
+};
